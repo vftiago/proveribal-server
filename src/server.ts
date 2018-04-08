788d54975dc7a1d982331def6d8f1b52d71e03b9
vftiago <vf.tiago@gmail.com>
2018-04-08 18:27:56 +0100

    clean(core): update license, package.json and tsconfig.json

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
 import { connect } from "mongodb";
 
 connect("mongodb://localhost:27017/proverbial", function(err, client) {
-	if (err) throw err;
+    if (err) throw err;
 
-	const db = client.db();
+    const db = client.db();
 
-	db
-		.collection("proverbs")
-		.find({ lang: "en" })
-		.toArray(function(err, result) {
-			if (err) throw err;
+    db
+        .collection("proverbs")
+        .find({ lang: "en" })
+        .toArray(function(err, result) {
+            if (err) throw err;
 
-			console.log(result);
-		});
+            console.log(result);
+        });
 });
