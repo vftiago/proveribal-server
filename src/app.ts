208a080dc11a9ee882c3dcc9ca00e1fd1c8b291b
vftiago <vf.tiago@gmail.com>
2018-05-12 19:43:26 +0100

    feat(core): use dotenv to store environment variables, connect directly to mlab

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
+import { config } from "dotenv";
 import { connect } from "mongoose";
 import * as express from "express";
 import * as cors from "cors";
 import Proverb from "./models/proverb";
 
-const port = 5000;
+config(); // retrieve .env file
+
+const env = process.env;
+const port = env.APP_PORT || 5000;
+const mongoURI = `mongodb://${env.DB_USER}:${env.DB_PASS}@${env.DB_HOST}:${
+    env.DB_PORT
+}/${env.DB_NAME}`;
+
 const app = express();
 const counts = {};
 
@@ -13,7 +21,7 @@ app.use(
     })
 );
 
-connect("mongodb://localhost/proverbial", err => {
+connect(mongoURI, err => {
     if (err) throw err;
     app.emit("ready");
     console.log(`Connected to Database at ${new Date().getMinutes()}`);
